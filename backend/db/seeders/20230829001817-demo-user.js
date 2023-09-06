f3df5910c3434492143a08bd6d0f4cd835b9f23a
Tayvor <tabeck98@gmail.com>
2023-09-06 17:24:19 -0600

    Pushing changes.

---
+++ backend/db/seeders/20230829001817-demo-user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'Demo',
+        lastName: 'the-demo',
+        email: 'demo@user.com',
+        username: 'Demo-Name',
+        hashedPassword: bcrypt.hashSync('password1')
+      },
+      {
+        firstName: 'Fake',
+        lastName: 'the-fake',
+        email: 'fake@user.com',
+        username: 'Fake-Name',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        firstName: 'False',
+        lastName: 'the-false',
+        email: 'false@user.com',
+        username: 'False-Name',
+        hashedPassword: bcrypt.hashSync('password3')
+      },
+    ], { validate: true });
+  },
+
+  async down(queryInterface, Sequelize) {
+    // options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-Name', 'Fake-Name', 'False-Name'] }
+    });
+  }
+};
