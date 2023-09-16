1451a3310ef49ccce903c0dacf914def7ff6f379
Tayvor <tabeck98@gmail.com>
2023-09-16 12:01:33 -0600

    Booking routes mostly completed

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,17 +3,17 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
+const bookingsRouter = require('./bookings.js')
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
-
-router.use('/spots', spotsRouter, reviewsRouter);
-
+router.use('/spots', spotsRouter, reviewsRouter, bookingsRouter);
 router.use('/reviews', reviewsRouter);
+router.use('/bookings', bookingsRouter);
+
 
 router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });
