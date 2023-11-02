ed267a075aef4223a50c56ed1c5e4c95e2b57fcd
Tayvor <tabeck98@gmail.com>
2023-11-02 16:46:46 -0600

    Auth Me phase-0

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -19,10 +19,9 @@ router.use('/spot-images', spotImagesRouter);
 router.use('/review-images', reviewImagesRouter);
 
 
-router.post('/test', function (req, res) {
-  res.json({ requestBody: req.body });
-});
-
+// router.post('/test', function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
 
 // router.get(
